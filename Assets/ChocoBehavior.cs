d4ddb6c9dbca64c2af1802fe4ffc5103fbb0fea5
León Mora <leonmora79@gmail.com>
2020-05-12 20:54:09 -0500

    Player and Enemy movement and fight without effects

---
+++ Assets/ChocoBehavior.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChocoBehavior : MonoBehaviour
+{
+    public float speed = 0.1f;
+    Rigidbody2D rb;
+    float posInicial;
+    float posActual;
+    int cambio = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        posInicial = transform.position.x;
+        posActual = posInicial + 0.1f;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(posActual >= (posInicial + 2) && cambio == 0)
+        {
+            speed = speed * -1;
+            cambio = 1;
+        }else if(posActual < (posInicial - 2) && cambio == 1)
+        {
+            speed = speed * -1;
+            cambio = 0;
+        }
+        rb.velocity = Vector2.right * speed;
+        posActual = transform.position.x;
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player" && collision.transform.position.y > transform.position.y+0.5)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 2.5f;
+    public int force = 370;
+    public int isGrounded;
+
+    public Rigidbody2D rb;
+    public SpriteRenderer spr;
+    public Animator animator;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        isGrounded = 0;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetAxis("Horizontal") == 0)
+        {
+            animator.SetBool("isWalking", false);
+        }
+        else if (Input.GetAxis("Horizontal") >= 1)
+        {
+            if(isGrounded == 1) { 
+                spr.flipX = false;
+                animator.SetBool("isWalking", true);
+            }
+        }
+
+        else if (Input.GetAxis("Horizontal") <= -1)
+        {
+            if (isGrounded == 1){
+                spr.flipX = true;
+                animator.SetBool("isWalking", true);
+            }
+        }
+
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rb.velocity.y);
+
+        if (Input.GetButtonDown(KeyCode.Space.ToString()) && isGrounded == 1){
+
+            animator.SetTrigger("isJumping"); //debe ir antes de la ejecución del salto para que se vea
+            animator.SetBool("isWalking", false);
+            isGrounded = 0;
+            rb.AddForce(Vector2.up * force);
+        
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "terrain")
+        {
+            isGrounded = 1;
+        }
+    }
+}
